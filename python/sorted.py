a4e8b49e4fe14cff4bc457dda1b162fae54f837a
Sufiyan Khan <sufiikhan980@gmail.com>
2025-08-13 12:50:47 +0500

    Added sorted.py

---
+++ python/sorted.py
@@ -0,0 +1,10 @@
+names = ["Carlos", "Ray", "Alex", "Kelly"]
+names.sort()
+print(names)
+print(sorted(names))
+print(sorted(names, key=len))
+
+
+#print(names.sort()) we can not write this way
+# sort() is a method and sorted() is a function
+
